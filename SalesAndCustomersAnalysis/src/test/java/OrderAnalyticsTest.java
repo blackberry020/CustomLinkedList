162da0b613d9e6cff60db0de066f818b3c25fdbd
Valentine Korneliuk <valentine.korneliuk17@gmail.com>
2025-06-09 12:43:16 +0300

    added concrete imports

--- SalesAndCustomersAnalysis/src/test/java/OrderAnalyticsTest.java
+++ SalesAndCustomersAnalysis/src/test/java/OrderAnalyticsTest.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OrderAnalyticsTest {
 
